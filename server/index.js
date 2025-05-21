22bee62f30a9599d18d3bae35f7e3e24125ca5b5
alif <aliftareq@gmail.com>
2025-05-21 20:15:56 +0600

    created user router and controller file

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import userRouter from './routes/user.route.js'
+
 dotenv.config();
 
 mongoose
@@ -20,3 +22,7 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000!!!");
 });
+
+//api's
+
+app.use('/api/user', userRouter)
